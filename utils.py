bf63d713ff3b048814bc90a9d427e43114ed2c2d
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-12-08 19:52:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -771,9 +771,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 [
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                            ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")
-                                ]
+                            ]
                             ]
                         )
                     )
@@ -814,9 +812,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         [
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                    ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")
-                        ]
+                    ]
                     ]
                 )
             )'''

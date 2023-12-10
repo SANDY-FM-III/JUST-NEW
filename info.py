e42dfc3c6567d153f37940ce712c884641f918d3
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-12-10 12:06:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bhartisandip514:er2IvvnmQjU1JRdG@cluster0.ouxlgp2.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "sandman")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
